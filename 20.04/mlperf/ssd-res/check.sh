6cd475f123bcb51e1050f73a26d427a9dfce27a5
Huntersz <azfarpro@gmail.com>
2021-07-06 16:52:26 +0800

    .

--- 20.04/mlperf/ssd-res/check.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-cp * /home/is-10/Desktop/IS_10/20.04/mlperf/ssd-res
-cd /home/is-10/Desktop/IS_10/20.04/mlperf/ssd-res
-git pull
-git add *
-git commit -am "hourly update"
-git push
-

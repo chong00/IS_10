24aab570a6f7fb1ae0a7b661e6d4206f795b51ee
Huntersz <azfarpro@gmail.com>
2021-07-13 18:15:08 +0800

    xhpl result update2

--- 20.04/xhpl/hpl-2.0_FERMI_v15/bin/is10/temphashcatscript.sh
+++ 20.04/xhpl/hpl-2.0_FERMI_v15/bin/is10/temphashcatscript.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-
+cd /home/is-10/Desktop/IS_10/20.04/hashcat/output/
 for i in {1..10}
 do
 	echo "#$i run start"
-	hashcat -b > /home/is-10/Desktop/IS_10/20.04/hashcat/output/hashcatnogui\-$i
+	hashcat -b > hashcat-riser-cables-$i
 	git add *
-	git commit -am "update"
+	git commit -am "hashcat update $i"
 	git push
 	echo "#$i run end"
 done

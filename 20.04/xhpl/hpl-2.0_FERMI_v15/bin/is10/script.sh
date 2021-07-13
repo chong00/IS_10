71f07d73546673e5bb0c7c4d12c3c31ea08b495f
Huntersz <azfarpro@gmail.com>
2021-07-13 17:17:47 +0800

    .

--- 20.04/xhpl/hpl-2.0_FERMI_v15/bin/is10/script.sh
+++ 20.04/xhpl/hpl-2.0_FERMI_v15/bin/is10/script.sh
@@ -3,12 +3,12 @@
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/is-10/Desktop/IS_10/20.04/xhpl/hpl-2.0_FERMI_v15/src/cuda
 ldd xhpl
 
-for i in {1..10}
+for i in {2..10}
 do
 	echo "#$i run start"
-	mpirun -np 4 xhpl > ../../../output/xhpl\-gui\-$i
-	git add ../../../output/*
-	git commit -am "xhpl result update"
+	mpirun -np 4 xhpl > ../../../riseroutput/xhpl\-gui\-$i
+	git add ../../../riseroutput/*
+	git commit -am "xhpl result update$i"
 	git push
 	echo "#$i run end"
 done
